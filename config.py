4346002d1215bfb6b05bfdf9bc6ac6be234ff02a
Sashas240 <ruslanbortnik70@gmail.com>
2025-08-11 12:18:56 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7550166794:AAGjNN_XwBLBMSeMLOs-IZtRHm5tQ8fGzm8")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "7142528797"))
 GPT_PROMPT = "Привет, этот бот передаёт NFT подарок, и не тратит на это 25 звёзд"
